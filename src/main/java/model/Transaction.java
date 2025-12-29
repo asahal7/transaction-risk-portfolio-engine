551fe5d7ec1526599b4cbbfe38e4ece589f8e7eb
Abdimaalik Sahal <abdimaaaalik0@gmail.com>
2025-12-29 10:42:34 +0000

    Validate Transaction constructor inputs

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -7,11 +7,24 @@ public class Transaction {
     private String assetname;
     private double amount; // value of transaction
 
-    public Transaction(Type type, String assetname, double amount) {
+   public Transaction(Type type, String assetname, double amount) {
+        if (type == null) {
+            throw new IllegalArgumentException("Transaction type cannot be null");
+        }
+
+        if (assetname == null || assetname.equals("")) {
+            throw new IllegalArgumentException("Asset name must not be null or an empty String, enter a valid assetname");
+        }
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than zero");
+        }
+
         this.type = type;
         this.assetname = assetname;
         this.amount = amount;
     }
+
 
     public Type getType() {
         return type;
